1fbf1d50cb0593d2c4017c2e3a12a8e944752b46
Sharandeep Singh <129933469+Sharan-deepSingh@users.noreply.github.com>
2024-01-16 12:04:27 +0530

    Initial Commit

---
+++ Barcode Scanner/Barcode_ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Barcode_ScannerApp.swift
+//  Barcode Scanner
+//
+//  Created by Sharandeep Singh on 16/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Barcode_ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
